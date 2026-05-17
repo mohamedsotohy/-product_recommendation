e353a0bca43c291da52184492ab3effc3836ff0b
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2026-05-17 21:53:19 +0300

    fix: cors error

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import SessionLocal
 from app.recommendation import recommender
 from app.routers import orders, products, recommendations
 
 app = FastAPI(title="Ecommerce Recommendation API", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
